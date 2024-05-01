a70778bf271e0c911ee0453078fcee07cede6c7b
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-01 10:29:34 -0700

    Test decrement and increment methods of ViewModel

--- app/src/test/java/edu/fullerton/csu/astronomypictureoftheday/APOD_ViewModelTest.kt
+++ app/src/test/java/edu/fullerton/csu/astronomypictureoftheday/APOD_ViewModelTest.kt
@@ -1,5 +1,50 @@
 package edu.fullerton.csu.astronomypictureoftheday
 
+import androidx.lifecycle.SavedStateHandle
 import org.junit.Assert.*
+import org.junit.Test
+import java.util.Calendar
+import java.util.GregorianCalendar
 
-class APOD_ViewModelTest
+class APOD_ViewModelTest{
+    @Test
+    fun addsDayToCalendar(){
+        var savedStatehandle = SavedStateHandle()
+        var apodViewModel = APOD_ViewModel(savedStatehandle)
+
+        val initialDate = apodViewModel.currentDate.clone() as Calendar
+
+        // Expected date is the current day plus one
+        val expectedDate = initialDate.clone() as Calendar
+        expectedDate.add(Calendar.DAY_OF_MONTH, 1)
+
+        // Call the method to be tested
+        apodViewModel.incrementDate()
+
+        // Assert that the currentDate has been incremented correctly
+        assertEquals(
+            expectedDate.get(Calendar.DAY_OF_MONTH),
+            apodViewModel.currentDate.get(Calendar.DAY_OF_MONTH)
+        )
+    }
+
+    @Test
+    fun decrementsDayToCalendar(){
+        var savedStatehandle = SavedStateHandle()
+        var apodViewModel = APOD_ViewModel(savedStatehandle)
+
+        val initialDate = apodViewModel.currentDate.clone() as Calendar
+
+        // Expected date is the current day plus one
+        val expectedDate = initialDate.clone() as Calendar
+        expectedDate.add(Calendar.DAY_OF_MONTH, -1)
+
+        // Call the method to be tested
+        apodViewModel.decrementDate()
+
+        assertEquals(
+            expectedDate.get(Calendar.DAY_OF_MONTH),
+            apodViewModel.currentDate.get(Calendar.DAY_OF_MONTH)
+        )
+    }
+}
