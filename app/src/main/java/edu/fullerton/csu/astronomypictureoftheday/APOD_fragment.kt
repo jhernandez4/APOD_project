0d2270699a9a12aa98e5ecb39de7807440dee86d
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-01 18:10:02 -0700

    Connect listeners to ViewModel functions

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/APOD_fragment.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/APOD_fragment.kt
@@ -1,13 +1,21 @@
 package edu.fullerton.csu.astronomypictureoftheday
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import edu.fullerton.csu.astronomypictureoftheday.databinding.FragmentApodBinding
+import java.util.Calendar
+
+private const val TAG = "APOD_fragment"
 
 class APOD_fragment : Fragment() {
+
+    private val dateViewModel: APOD_ViewModel by viewModels()
 
     // create binding for xml file -> binding class made automatically by enabling ViewBinding
     private lateinit var binding: FragmentApodBinding
@@ -33,16 +41,41 @@ class APOD_fragment : Fragment() {
         binding.apply {
 
             btnPrev.setOnClickListener {
-                TODO("Connect with ViewModel")
+                dateViewModel.decrementDate()
+                Log.d(TAG, "Decremented date by 1: ${dateViewModel.currentDate.time}")
+                // update picture and description,author,etc from NASA according to
+                // dateViewModel.currentDate.get(Calendar.YEAR)
+                // dateViewModel.currentDate.get(Calendar.MONTH)
+                // dateViewModel.currentDate.get(Calendar.DAY_OF_MONTH)
+                // call updateAPOD()
             }
 
             btnNext.setOnClickListener {
-                TODO("Connect with ViewModel")
+                dateViewModel.incrementDate()
+                Log.d(TAG, "Incremented date by 1: ${dateViewModel.currentDate.time}")
+                // update picture and description,author,etc from NASA
             }
 
             btnDatePicker.setOnClickListener {
-                TODO("Connect with ViewModel")
+                // get date from date selected by user
+                // set the date with -> dateViewModel.setDate(year, month, day)
+
+                // hard-coded values to test setDate functionality
+                // this is the first day an APOD was posted by NASA
+                val year = 1995
+                val month = 6
+                val day = 16
+
+                dateViewModel.setDate(year, month, day)
+                Log.d(TAG, "Date set by user: ${dateViewModel.currentDate.time}")
+                // update picture and description,author,etc from NASA with new date
             }
         }
     }
+
+    // function signature can be changed
+    fun updateAPOD(year: Int, month: Int, day: Int) {
+        // update ImageView and Text from NASA's api using the date passed in
+    }
+
 }
