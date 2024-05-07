3a9b57ebad18c5fb3b7c71aee4775506a602e7f7
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-06 23:55:13 -0700

    Load picture in coroutine in OnStart state

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/APOD_fragment.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/APOD_fragment.kt
@@ -16,7 +16,11 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import android.net.Uri
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
+import kotlinx.coroutines.launch
 import java.util.Properties
 
 private const val TAG = "APOD_fragment"
@@ -47,47 +51,26 @@ class APOD_fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Load the API key in Fragment
-        val apiKey = loadApiKey(requireContext())
-        dateViewModel.setApiKeyFromContext(requireContext())
-        // Call the fetchPicture method with context
-        binding.btnSelectDate.setOnClickListener {
-            dateViewModel.fetchPicture()
-        }
-        observePicture()
-        // Observing the currentPicture LiveData from the ViewModel
-        dateViewModel.currentPicture.observe(viewLifecycleOwner) { astronomyPicture ->
-            // Ensure the astronomyPicture is not null
-            astronomyPicture?.let {
-                // Using Glide to load the image from URL into imageView
-                Glide.with(this@APOD_fragment) // Ensure you use the Fragment context correctly
-                    .load(it.url) // URL from the LiveData
-                    .placeholder(R.drawable.placeholder_background) // Placeholder image
-                    .error(R.drawable.error_image_background) // Error image
-                    .into(binding.imageView) // Target imageView
 
-                // Set the text of the description TextView
-                binding.descriptionText.text = it.explanation
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                // Load the API key in Fragment
+                val apiKey = loadApiKey(requireContext())
+                dateViewModel.setApiKeyFromContext(requireContext())
+                // Call the fetchPicture method with context
+                observePicture()
             }
         }
 
         binding.apply {
-
             btnPrev.setOnClickListener {
                 dateViewModel.decrementDate()
-                // update picture and description,author,etc from NASA according to
-                // dateViewModel.currentDate.get(Calendar.YEAR)
-                // dateViewModel.currentDate.get(Calendar.MONTH)
-                // dateViewModel.currentDate.get(Calendar.DAY_OF_MONTH)
-                // call updateAPOD()
             }
 
             btnNext.setOnClickListener {
                 dateViewModel.incrementDate()
                 // update picture and description,author,etc from NASA
             }
-
-
 
             btnSelectDate.setOnClickListener {
                 // get date from date selected by user
@@ -102,7 +85,7 @@ class APOD_fragment : Fragment() {
 
                 dateViewModel.setDate(year, month, day)
                 Log.d(TAG, "Date set by user: ${dateViewModel.currentDate.time}")
-                // update picture and description,author,etc from NASA with new date
+                dateViewModel.fetchPicture()
             }
         }
     }
@@ -133,12 +116,5 @@ class APOD_fragment : Fragment() {
                 binding.tvTitle.text = it.title
             }
         }
-
-
-        // function signature can be changed
-        fun updateAPOD(year: Int, month: Int, day: Int) {
-            // update ImageView and Text from NASA's api using the date passed in
-        }
-
     }
 }
