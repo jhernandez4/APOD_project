7fd1493e808e732a79a81f06ebbeb3a7889a74e6
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 13:48:21 -0700

    Initialize list of favorite pictures

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListViewModel.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListViewModel.kt
@@ -1,4 +1,21 @@
 package edu.fullerton.csu.astronomypictureoftheday
 
-class FavoriteListViewModel {
+import androidx.lifecycle.ViewModel
+import java.util.Date
+import java.util.UUID
+
+class FavoriteListViewModel : ViewModel() {
+    val favorites = mutableListOf<Favorite>()
+
+    init {
+        for (i in 0 until 100) {
+            val favorite = Favorite(
+                id = UUID.randomUUID(),
+                title = "Favorite Picture #$i",
+                date = Date()
+            )
+
+            favorites += favorite
+        }
+    }
 }
