4b29459a1845358e4b9c20ebda82d33cfdf7c1b2
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 13:49:02 -0700

    Add Fragment boilerplate

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListFragment.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListFragment.kt
@@ -1,4 +1,17 @@
 package edu.fullerton.csu.astronomypictureoftheday
 
-class FavoriteListFragment {
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+private const val TAG = "FavoriteListFragment"
+class FavoriteListFragment : Fragment() {
+
+    private val favoriteListViewModel: FavoriteListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${favoriteListViewModel.favorites.size}")
+    }
 }
