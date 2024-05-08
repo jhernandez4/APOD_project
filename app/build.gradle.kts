3aa1eb8a52baf9112933c318f7d9360d68afd70f
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-07 20:14:19 -0700

    Add safeargs plug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("androidx.navigation.safeargs.kotlin") version "2.5.0" apply false
 }
 
 android {
@@ -34,9 +35,6 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures{
-        viewBinding = true
-    }
-    buildFeatures {
         viewBinding = true
     }
 }
