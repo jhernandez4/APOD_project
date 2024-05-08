6d6b2efa586d60b8c1e54c0dafbed023a8badef1
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 13:54:07 -0700

    Add recyclerview dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation(libs.junit)
     testImplementation("junit:junit:4.12")
