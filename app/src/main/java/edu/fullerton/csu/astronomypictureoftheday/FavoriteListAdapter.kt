d5e4548544e8255f42b33868e5b57f3167def35a
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 14:27:52 -0700

    Create adapter for recycler view

---
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListAdapter.kt
@@ -0,0 +1,29 @@
+package edu.fullerton.csu.astronomypictureoftheday
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import edu.fullerton.csu.astronomypictureoftheday.databinding.ListItemFavoriteBinding
+
+class FavoriteHolder (val binding: ListItemFavoriteBinding)
+    : RecyclerView.ViewHolder(binding.root){
+}
+class FavoriteListAdapter(
+    private val favorites: List<Favorite>
+) : RecyclerView.Adapter<FavoriteHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemFavoriteBinding.inflate(inflater, parent, false)
+        return FavoriteHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteHolder, position: Int) {
+        val favorite = favorites[position]
+        holder.apply {
+            binding.favoriteTitle.text = favorite.title
+            binding.favoriteDate.text = favorite.date.toString()
+        }
+    }
+
+    override fun getItemCount() = favorites.size
+}
