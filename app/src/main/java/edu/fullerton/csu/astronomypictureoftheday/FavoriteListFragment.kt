a67c0537e401d9c0eb66f0a15338b27ce1ad8a48
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 14:01:58 -0700

    Inflate and bind fragment layout

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListFragment.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListFragment.kt
@@ -2,16 +2,39 @@ package edu.fullerton.csu.astronomypictureoftheday
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import edu.fullerton.csu.astronomypictureoftheday.databinding.FragmentFavoriteListBinding
 
 private const val TAG = "FavoriteListFragment"
 class FavoriteListFragment : Fragment() {
 
+    private var _binding: FragmentFavoriteListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
     private val favoriteListViewModel: FavoriteListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${favoriteListViewModel.favorites.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoriteListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
