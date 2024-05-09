8454344d1521a65624dff881bb5abe2875d40dae
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 21:20:57 -0700

    Define queries

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/database/FavoriteDao.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/database/FavoriteDao.kt
@@ -1,4 +1,25 @@
 package edu.fullerton.csu.astronomypictureoftheday.database
 
-class FavoriteDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import edu.fullerton.csu.astronomypictureoftheday.Favorite
+import java.util.Calendar
+
+@Dao
+interface FavoriteDao {
+    @Query("SELECT * FROM favorite")
+    suspend fun getFavorites(): List<Favorite>
+
+    // date is  in MM/DD/YYYY -> month range is from 1-12
+    // function returns 1 if exists, 0 if otherwise
+    @Query("SELECT COUNT(*) FROM Favorite WHERE date=(:date)")
+    fun getFavoriteCount(date: String): Int
+
+    @Query("DELETE FROM favorite WHERE date=(:date)")
+    suspend fun deleteFavorite(date: String)
+
+    @Insert
+    suspend fun addFavorite(favorite: Favorite)
 }
