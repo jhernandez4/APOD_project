4eed61f6d2948fe04012f6b57452494dbce484c4
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 21:21:49 -0700

    Create repository to build database file

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteRepository.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteRepository.kt
@@ -1,4 +1,40 @@
 package edu.fullerton.csu.astronomypictureoftheday
 
-class FavoriteRepository {
+import android.content.Context
+import androidx.room.Room
+import edu.fullerton.csu.astronomypictureoftheday.database.FavoriteDatabase
+import java.lang.IllegalStateException
+
+private const val DATABASE_NAME = "favorite-database"
+
+class FavoriteRepository private constructor(context: Context){
+    private val database: FavoriteDatabase = Room
+        .databaseBuilder(
+            context.applicationContext,
+            FavoriteDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+
+    suspend fun getFavorites(): List<Favorite> = database.favoriteDao().getFavorites()
+
+    suspend fun getFavoriteCount(date: String): Int = database.favoriteDao().getFavoriteCount(date)
+
+    suspend fun deleteFavorite(date: String) = database.favoriteDao().deleteFavorite(date)
+
+    suspend fun addFavorite(favorite: Favorite) = database.favoriteDao().addFavorite(favorite)
+
+    companion object{
+        private var INSTANCE: FavoriteRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = FavoriteRepository(context)
+            }
+        }
+
+        fun get(): FavoriteRepository {
+            return INSTANCE ?:
+            throw IllegalStateException("CrimeRepository must be initialized")
+        }
+    }
 }
