e5bf4397a0ecd32414ddb5701a08f019078b27f7
jhernandez4 <jokhernandez120744@gmail.com>
2024-05-08 22:09:32 -0700

    Forbid schema export

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/database/FavoriteDatabase.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/database/FavoriteDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import edu.fullerton.csu.astronomypictureoftheday.Favorite
 
-@Database(entities = [Favorite::class], version=1)
+@Database(entities = [Favorite::class], version=1, exportSchema = false)
 @TypeConverters(FavoriteTypeConverters::class)
 abstract class FavoriteDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
