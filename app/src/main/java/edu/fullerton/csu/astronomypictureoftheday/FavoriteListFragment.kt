fd78208cff96c32079690234d9502a0d67f8f19b
EthanJDunn <edunn9@csu.fullerton.edu>
2024-05-18 15:59:05 -0700

    addition files for datastore

--- app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListFragment.kt
+++ app/src/main/java/edu/fullerton/csu/astronomypictureoftheday/FavoriteListFragment.kt
@@ -3,9 +3,16 @@ package edu.fullerton.csu.astronomypictureoftheday
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -15,6 +22,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import edu.fullerton.csu.astronomypictureoftheday.databinding.FragmentFavoriteListBinding
 import kotlinx.coroutines.launch
+
 
 private const val TAG = "FavoriteListFragment"
 class FavoriteListFragment : Fragment() {
@@ -26,6 +34,9 @@ class FavoriteListFragment : Fragment() {
         }
     private val favoriteListViewModel: FavoriteListViewModel by viewModels()
 
+    private lateinit var datastore: Datastore
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,6 +45,7 @@ class FavoriteListFragment : Fragment() {
         _binding = FragmentFavoriteListBinding.inflate(inflater, container, false)
 
         binding.favoriteRecyclerView.layoutManager = LinearLayoutManager(context)
+        datastore = Datastore.getInstance(requireContext())
 
         return binding.root
     }
@@ -65,6 +77,30 @@ class FavoriteListFragment : Fragment() {
                 }
 
             }
+        }
+        val lightSwitch = binding.root.findViewById<Switch>(R.id.lightSwitch)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                datastore.uiMode.collect { isNightMode ->
+                    setUIMode(isNightMode)
+                    lightSwitch.isChecked = isNightMode
+                }
+            }
+
+            lightSwitch.setOnCheckedChangeListener { _, isChecked ->
+                setUIMode(isChecked)
+                viewLifecycleOwner.lifecycleScope.launch {
+                    datastore.saveToDataStore(isChecked)
+                }
+            }
+        }
+    }
+    private fun setUIMode(isNightMode: Boolean) {
+        if (isNightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 
